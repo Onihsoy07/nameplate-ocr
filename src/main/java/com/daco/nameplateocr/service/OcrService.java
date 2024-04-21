6f8890e414be204ea2709248f662382fdeb884da
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2024-04-21 17:41:42 +0900

    이미지 그레이 스케일 변환

--- src/main/java/com/daco/nameplateocr/service/OcrService.java
+++ src/main/java/com/daco/nameplateocr/service/OcrService.java
@@ -6,14 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import java.awt.image.DataBufferByte;
-
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.RescaleOp;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -47,15 +38,14 @@ public class OcrService {
         String storeFileName = createStoreFileName(originalFilename);
         String fileFullPath = getFullPath(storeFileName);
         File originalImageFile = new File(fileFullPath);
-        String imagePretreatmentFullPath = getImagePretreatmentFullPath(originalFilename);
+        String imagePretreatmentFullPath = getImagePretreatmentFullPath(storeFileName);
 
         // multipartFile을 storeFileName으로 저장
         multipartFile.transferTo(originalImageFile);
 
         // 이미지 전처리 후 파일 반환
-        getConvertImageGrayScale(originalImageFile, imagePretreatmentFullPath);
+        getConvertImageGrayScale(originalImageFile, imagePretreatmentFullPath, extractedExt(imagePretreatmentFullPath));
         File imagePretreatmentFile = new File(imagePretreatmentFullPath);
-//        File imagePretreatmentFile = getImagePretreatmentFile(originalImageFile);
 
 
         // Tesseract 객체 생성
@@ -65,7 +55,7 @@ public class OcrService {
         tesseract.setDatapath(TESSDATA);
 
         // 영어(eng) 학습 데이터 선택
-        tesseract.setLanguage("kor");
+        tesseract.setLanguage("eng+kor");
 
         // OCR 작업 후 결과 반환
         String result = tesseract.doOCR(imagePretreatmentFile);
@@ -106,71 +96,20 @@ public class OcrService {
     }
 
     // 이미지 그레이 스케일 변환
-    private void getConvertImageGrayScale(File file, String imagePretreatmentFilePath) throws IOException {
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-//        Mat imageLenna = Imgcodecs.imread(originalFilePath);
-
+    private void getConvertImageGrayScale(File file, String imagePretreatmentFilePath, String ext) throws IOException {
         BufferedImage image = ImageIO.read(file);
 
-        byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        Mat imageLenna = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
-        imageLenna.put(0, 0, data);
-
-
-        // GrayScale
-        Imgproc.cvtColor(imageLenna, imageLenna, Imgproc.COLOR_BayerRG2GRAY);
-
-        Imgcodecs.imwrite(imagePretreatmentFilePath, imageLenna);
-    }
-
-    // 이미지 전처리(노이즈 제거)
-    private void ImageProcess(BufferedImage Input_Image, float scaleFactor, float offset,
-                              String fileFullPath, String extension) throws IOException {
-        // First of all, create an empty image buffer
-        BufferedImage Output_Image = new BufferedImage(1050, 1024, Input_Image.getType());
-
-        // Now create a 2D platform
-        Graphics2D DemoGraphic = Output_Image.createGraphics();
-
-        // draw a new image
-        DemoGraphic.drawImage(Input_Image, 0, 0, 1050, 1024, null);
-        DemoGraphic.dispose();
-
-        // rescale the OP object for the grey images
-        RescaleOp RescaleImage = new RescaleOp(scaleFactor, offset, null);
-
-        // perform scaling
-        BufferedImage Buffered_FOP_Image = RescaleImage.filter(Output_Image, null);
-        ImageIO.write(Buffered_FOP_Image, extension,
-                new File(fileFullPath));
-    }
-
-    // 조건별 이미지 전처리 작업
-    private File getImagePretreatmentFile(File file) throws IOException {
-        String imagePretreatmentFilePath = getImagePretreatmentFullPath(file.getName());
-        String extension = extractedExt(imagePretreatmentFilePath);
-        BufferedImage Input_Image = ImageIO.read(file);
-
-        double Image_Double =
-                Input_Image.getRGB(Input_Image.getTileWidth() / 2, Input_Image.getTileHeight() / 2);
-
-        // compare the values
-        if (Image_Double >= -1.4211511E7 && Image_Double < -7254228) {
-            ImageProcess(Input_Image, 3f, -10f, imagePretreatmentFilePath, extension);
-        } else if (Image_Double >= -7254228 && Image_Double < -2171170) {
-            ImageProcess(Input_Image, 1.455f, -47f, imagePretreatmentFilePath, extension);
-        } else if (Image_Double >= -2171170 && Image_Double < -1907998) {
-            ImageProcess(Input_Image, 1.35f, -10f, imagePretreatmentFilePath, extension);
-        } else if (Image_Double >= -1907998 && Image_Double < -257) {
-            ImageProcess(Input_Image, 1.19f, 0.5f, imagePretreatmentFilePath, extension);
-        } else if (Image_Double >= -257 && Image_Double < -1) {
-            ImageProcess(Input_Image, 1f, 0.5f, imagePretreatmentFilePath, extension);
-        } else if (Image_Double >= -1 && Image_Double < 2) {
-            ImageProcess(Input_Image, 1f, 0.35f, imagePretreatmentFilePath, extension);
+        for(int y = 0; y < image.getHeight(); y++) {
+            for(int x = 0; x < image.getWidth(); x++) {
+                Color colour = new Color(image.getRGB(x, y));
+//       Choose one from below
+//       int Y = (int) (0.299 * colour.getRed() + 0.587 * colour.getGreen() + 0.114 * colour.getBlue());
+                int Y = (int) (0.2126 * colour.getRed() + 0.7152 * colour.getGreen() + 0.0722 * colour.getBlue());
+                image.setRGB(x, y, new Color(Y, Y, Y).getRGB());
+            }
         }
 
-        return new File(imagePretreatmentFilePath);
+        ImageIO.write(image, ext, new File(imagePretreatmentFilePath));
     }
 
 }
