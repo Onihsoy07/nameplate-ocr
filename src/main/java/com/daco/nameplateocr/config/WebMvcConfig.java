43d9ca8d1919401508233f9222037fdfea0fa7d3
박준혁 <84126411+Onihsoy07@users.noreply.github.com>
2024-04-30 12:21:33 +0900

    CORS 설정

---
+++ src/main/java/com/daco/nameplateocr/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.daco.nameplateocr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 모든 CORS 허용
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(6000);
+    }
+
+}
